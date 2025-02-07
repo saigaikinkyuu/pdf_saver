7937b9d90d97268d0a45582dd5ac463ae4277215
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-02-07 20:38:28 +0900

    Update index.js

--- files/index.js
+++ files/index.js
@@ -4,10 +4,12 @@ const urlParams = new URLSearchParams(queryString);
 // クエリパラメータ "name" を取得する
 const id = urlParams.get('id');
 const name = urlParams.get('name');
+let flag = false
 
 fetch("../files.json")
   .then(response => response.json())
   .then(data => {
+    let datas = data.active
     fetch("./data.json")
       .then(response => response.json())
       .then(items => {
@@ -15,8 +17,20 @@ fetch("../files.json")
         if(let i = 0;i<item.length;i++){
           if(item[i].key === id && item[i].url === name){
             i += item.length
-            document.body.innerHTML = "<iframe src='../?req=sh&name=none&url=" + item[i].hrf + "' style='width: 100%;height: 100%;'></iframe>"
+            for(let s = 0;s<datas.length;s++){
+              if(datas[s].url === name){
+                s += datas.length
+                if(datas[s].sta === "available"){
+                  document.body.innerHTML = "<iframe src='../?req=sh&name=none&url=" + item[i].hrf + "' style='width: 100%;height: 100%;'></iframe>"
+                  flag = true
+                }
+              }
+            }
           }
         }
       })
   })
+
+if(flag === false){
+  window.location.href = "../error/?code=10001&num=null"
+}
