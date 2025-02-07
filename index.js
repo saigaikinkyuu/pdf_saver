9870877ff9840dee58a0d8aa08acdeb21f7e34b2
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-02-07 18:58:46 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,54 @@
+// 現在のURLからクエリパラメータを取得する
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+// クエリパラメータ "name" を取得する
+const request_flag = urlParams.get('req');
+const file_name = urlParams.get('name');
+let flag = false
+if(request_flag && file_name){
+  flag = true
+}
+
+if(flag === true){
+  if(request_flag === "get"){
+    fetch("./files.json")
+      .then(response => response.json())
+      .then(data => {
+        let files_ac = data.active
+        let length_fi = files_ac.length
+        let check_flag = false
+        let file_info = []
+        for(let i = 0;i<length_fi;i++){
+          if(check_flag === true){
+            i += length_fi
+          }else {
+            if(files_ac[i].id === file_name){
+              check_flag = true
+              file_info.push(files_ac[i].url,files_ac[i].sta,files_ac[i].memo,files_ac[i].date,files_ac[i].update,i)
+            }
+          }
+        }
+        if(check_flag){
+          if(file_info[1] === "available"){
+            if(file_info[2] !== "none"){
+              let memo_pre = (file_info[2]).replace("{{date}}",file_info[3])
+              let memo = memo_pre.replace("{{update}}",file_info[4])
+              let confirm = window.confirm(memo);
+              if(confirm === false){
+                alert("不明な理由で利用できません")
+                window.close()
+              }
+            }
+            window.location.href = "./" + file_info[0];
+          }else if(file_info[1] === "un_available"){
+            window.location.href = "./error/?code=00001&num=" + file_info[5];
+          }else if(file_info[1] === "limit"){
+            window.location.href = "./error/?code=00101&num=" + file_info[5];
+          }
+        }else {
+          window.location.href = "./error/?code=10001&";
+        }
+      })
+  }
+}
