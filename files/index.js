91754e59deb167f41db87aee7ba3e71508682005
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-02-08 18:57:47 +0900

    Update index.js

--- files/index.js
+++ files/index.js
@@ -53,6 +53,7 @@ fetch("../files.json")
                   document.title = "Contents"
                   flag = true
                   // a要素を作成してダウンロードを実行
+                  const filename = '数学.pdf'; // ダウンロードするファイル名
                   const url = "../files/ac/" + item[i].hrf
                   const a = document.createElement('a');
                   a.href = url;
